d4090ac015d2e599c25374880cac7b56292a9483
william <william@DESKTOP-OL0HJ5R>
2026-01-31 12:54:35 -0300

    refactor(Domain): Following files have been removed Challenge_UBS.Domain  Entities   Trade.cs  Interfaces   IRiskRules.cs

--- Challenge_UBS/Challenge_UBS.Domain/Entities/Trade.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Challenge_UBS.Domain.Entities;
-
-//Class used to represent a trade with client details and value.
-public class Trade
-{
-    public string ClientId { get; init; } = default!;
-    public decimal Value { get; init; }
-    public string ClientSector { get; init; } = default!;
-}
